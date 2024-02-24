9763166fb05bb077b6126dc0f5fb449930052716
SONALBAHETI <sonal.21mcan176@jecrcu.edu.in>
2024-02-24 12:18:33 +0530

    mentor page added

---
+++ src/app/mentor/allMentors/page.tsx
@@ -0,0 +1,15 @@
+import AboutMentor from "@/components/mentor/about/AboutMentor";
+import Education from "@/components/mentor/education/Education";
+import Reviews from "@/components/mentor/reviews/Reviews";
+import React from "react";
+
+export default function AllMentorPage() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <AboutMentor></AboutMentor>
+      <Education></Education>
+      <Reviews></Reviews>
+    </div>
+  );
+}
---
+++ src/components/mentor/reviews/Reviews.tsx
@@ -0,0 +1,14 @@
+import { Card, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+export default function Reviews() {
+  return (
+    <>
+      <div>
+        <Card>
+          <CardTitle className="text-md">Reviews</CardTitle>
+        </Card>
+      </div>
+    </>
+  );
+}

885c51b3351e09e1a6c0b198ce84b0bbb1f128ec
Pankaj Gurbani <pankajgurbani.connect@gmail.com>
2024-04-05 17:51:56 +0530

    Set default stale time to 30 seconds for all queries

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 30 * 1000, // 30 seconds
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       {children}

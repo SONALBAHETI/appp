30046dcb25c0d1e31b93fe170b3afe269d66281b
Pankaj Gurbani <pankajgurbani.connect@gmail.com>
2024-04-07 13:27:18 +0530

    Navigation TopBar improvements

--- src/components/ui/ProfilePicture/ProfilePicture.tsx
+++ src/components/ui/ProfilePicture/ProfilePicture.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../avatar";
 import Icon, { IconType } from "../Icon";
+import { Skeleton } from "../skeleton";
+import { cn } from "@/lib/utils";
 
 interface IProfilePictureProps {
   userName?: string;
   profilePic?: string;
+  loading?: boolean;
 }
 
 const ProfilePicture = React.forwardRef<
   React.ElementRef<typeof Avatar>,
   React.ComponentPropsWithoutRef<typeof Avatar> & IProfilePictureProps
->(({ className, userName, profilePic, ...props }, ref) => (
-  <Avatar className={className} ref={ref} {...props}>
-    <AvatarImage src={profilePic} alt="User profile picture" />
-    <AvatarFallback>
-      {userName ? userName[0] : <Icon type={IconType.USER} />}
-    </AvatarFallback>
-  </Avatar>
+>(({ className, userName, profilePic, loading, ...props }, ref) => (
+  <>
+    {loading ? (
+      <Skeleton className={cn("rounded-full", className)} />
+    ) : (
+      <Avatar className={className} ref={ref} {...props}>
+        <AvatarImage src={profilePic} alt="User profile picture" />
+        <AvatarFallback>
+          {userName ? userName[0] : <Icon type={IconType.USER} />}
+        </AvatarFallback>
+      </Avatar>
+    )}
+  </>
 ));
 ProfilePicture.displayName = Avatar.displayName;
 
